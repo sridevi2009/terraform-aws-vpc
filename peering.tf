8781d7941811661087c7d1f205f0eadb220479d3
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-01-13 10:49:53 +0530

    aws-vpc

--- peering.tf
+++ peering.tf
@@ -18,7 +18,6 @@ resource "aws_route" "acceptor_route" {
   route_table_id = data.aws_route_table.default.id
   destination_cidr_block = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-
 }
 
 resource "aws_route" "public_peering" {
@@ -26,7 +25,6 @@ resource "aws_route" "public_peering" {
   route_table_id = aws_route_table.public.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-
 }
 
 resource "aws_route" "private_peering" {
@@ -34,7 +32,6 @@ resource "aws_route" "private_peering" {
   route_table_id = aws_route_table.private.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-
 }
 
 resource "aws_route" "database_peering" {
@@ -42,5 +39,4 @@ resource "aws_route" "database_peering" {
   route_table_id = aws_route_table.database.id
   destination_cidr_block = data.aws_vpc.default.cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-
 }
