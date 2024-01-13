97eeb179293393f870d958e8857c4af16483effc
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-01-13 11:02:10 +0530

    aws-vpc

--- main.tf
+++ main.tf
@@ -67,6 +67,15 @@ resource "aws_subnet" "database" {
   )
 }
 
+resource "aws_subnet" "database" {
+  name = "${local.name}"
+  subnet_ids = aws_subnet.database[*].id
+  tags = {
+    name = "${local.name}"
+  } 
+    
+}
+
 resource "aws_eip" "eip" {
   domain  = "vpc"
 }
