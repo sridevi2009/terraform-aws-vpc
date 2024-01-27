705c14b84c579cd004737258844beb21c9c20975
sridevi2009 <tiruveedulasridevi8247477004@gmail.com>
2024-01-27 17:36:09 +0530

    practice

--- outputs.tf
+++ outputs.tf
@@ -6,14 +6,14 @@ output "vpc_id" {
     value = aws_vpc.main.id
 }
 
-output "public_subnets_ids" {
+output "public_subnet_ids" {
     value = aws_subnet.public[*].id
 }
 
-output "private_subnets_ids" {
+output "private_subnet_ids" {
     value = aws_subnet.private[*].id
 }
 
-output "database_subnets_ids" {
+output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
